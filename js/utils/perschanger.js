83c972036ea456779056de8aef1233c0cb79d537
RetrogisusDEV <gisuselpro123@hotmail.com>
2025-01-21 18:44:33 -0400

    Cambio en la estrutura del projecto

---
+++ js/utils/perschanger.js
@@ -0,0 +1,18 @@
+const foregroundImg = document.getElementById("foreground");
+
+const characters = {
+  1: "yuyuko",
+  2: "sagume"
+};
+
+// Fixing the syntax issue in the image source
+/* foregroundImg.innerHTML = `
+  <img 
+    src="media/${characters[1]}/foreground.png" 
+    id="foreground" 
+    alt="Foreground Image" 
+    loading="eager" 
+    aria-placeholder="Foreground Image"
+  >
+`;
+*/
